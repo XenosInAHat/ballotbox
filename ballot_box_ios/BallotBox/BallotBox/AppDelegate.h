91b09730bdb1576e85403b7eb066a1e1a93a9013
Brendan Cazier <admin@brendancazier.com>
2014-12-04 23:31:57 -0500

    Actually added barebones iOS app

---
+++ ballot_box_ios/BallotBox/BallotBox/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  BallotBox
+//
+//  Created by Brendan Cazier on 12/4/14.
+//  Copyright (c) 2014 BallotBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ballot_box_ios/BallotBox/BallotBox/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BallotBox
+//
+//  Created by Brendan Cazier on 12/4/14.
+//  Copyright (c) 2014 BallotBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
